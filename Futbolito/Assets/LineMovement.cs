bf3c547778bc34f85a7560727d0e454371de67b0
Jose Galvez <jjgalper@gmail.com>
2018-01-30 00:02:02 -0500

    Inicio de la programacion de las lineas

---
+++ Futbolito/Assets/LineMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineMovement : MonoBehaviour {
+    
+    //Line Speed. Depends on the number of players.
+    public float speed;
+    //Number of paddles in line.
+    public int numberPaddles;
+    //Paddle reference to spawn in line.
+    public GameObject pad;
+    //Screen width
+    float screenHalfWidthInWorldUnits;
+
+
+	
+	void Start () {
+
+        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize;
+        print(screenHalfWidthInWorldUnits);
+	}
+	
+	void Update () {
+        //Line movement
+        float xMov = Input.acceleration.x;
+        float velocity = xMov * speed;
+        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+
+
+	}
+}
