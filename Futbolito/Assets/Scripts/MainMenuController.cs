c64c848b780dee8f68aca3488214f5b589ee0594
José Juan Gálvez <jjgalper@gmail.com>
2018-06-18 17:30:50 -0500

    Team Scene

---
+++ Futbolito/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+	
+    public void ChangeScene(int indexScene)
+    {
+        SceneManager.LoadScene(indexScene);
+    }
+}
