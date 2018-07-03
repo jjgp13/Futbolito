75fb3100d70d5c0681859e8cf65b5f33e55cd1d4
José Juan Gálvez <jjgalper@gmail.com>
2018-07-03 16:52:33 -0500

    Select Team Scene

---
+++ Futbolito/Assets/Scripts/Team.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Team", menuName = "Team")]
+public class Team : ScriptableObject {
+
+    public string teamName;
+
+    public Sprite flag;
+    public Sprite formation;
+
+    public int defense;
+    public int midfield;
+    public int attack;
+
+    public AnimationClip[] animations;
+
+}
