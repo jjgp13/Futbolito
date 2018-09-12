74a2c39ec0a96a0c41a62d6042d0769a412c3dc6
José Juan Gálvez <jjgalper@gmail.com>
2018-09-12 16:43:05 -0500

    tours menu almost finished

--- Futbolito/Assets/Scripts/ToursMenuController.cs
+++ Futbolito/Assets/Scripts/ToursMenuController.cs
@@ -12,8 +12,16 @@ public class ToursMenuController : MonoBehaviour {
     public Button[] toursBtns;
     public Sprite selectedTourSprite, notSelectedTourSprite;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject teamsLayoutObj;
+    private GridLayoutGroup teamsLayout;
+
+    public Image tourMapSprite;
+    public Sprite[] tourMaps;
+    
+
+    // Use this for initialization
+    void Start () {
+        teamsLayout = teamsLayoutObj.GetComponent<GridLayoutGroup>();
 	}
 	
 	// Update is called once per frame
@@ -24,6 +32,9 @@ public class ToursMenuController : MonoBehaviour {
     public void DisplayTeamsOnPanel(int tourIndex)
     {
         DeleteTeamsFromPanel();
+        SetTeamsPanel(tours[tourIndex].teams.Length);
+        tourMapSprite.sprite = tourMaps[tourIndex];
+
         Tournament tour = tours[tourIndex];
         for (int i = 0; i < tour.teams.Length; i++)
         {
@@ -48,6 +59,37 @@ public class ToursMenuController : MonoBehaviour {
             if (index == i) toursBtns[index].image.sprite = selectedTourSprite;
             else toursBtns[i].image.sprite = notSelectedTourSprite;
         }
-        //toursBtns[index].image.sprite = selectedTourSprite;
+    }
+
+    void SetTeamsPanel(int teamsN)
+    {
+        if(teamsN == 16)
+        {
+            teamsLayout.padding = new RectOffset(20, 20, 20, 20);
+            teamsLayout.cellSize = new Vector2(128, 104);
+            teamsLayout.spacing = new Vector2(55, 15);
+            teamsLayout.startAxis = GridLayoutGroup.Axis.Vertical;
+        }
+        else if (teamsN == 12)
+        {
+            teamsLayout.padding = new RectOffset(20, 20, 20, 20);
+            teamsLayout.cellSize = new Vector2(128, 104);
+            teamsLayout.spacing = new Vector2(150, 15);
+            teamsLayout.startAxis = GridLayoutGroup.Axis.Vertical;
+        }
+        else if (teamsN == 24)
+        {
+            teamsLayout.padding = new RectOffset(20, 20, 20, 20);
+            teamsLayout.cellSize = new Vector2(104, 80);
+            teamsLayout.spacing = new Vector2(11, 45);
+            teamsLayout.startAxis = GridLayoutGroup.Axis.Vertical;
+        }
+        else
+        {
+            teamsLayout.padding = new RectOffset(8,6,20,20);
+            teamsLayout.cellSize = new Vector2(88, 64);
+            teamsLayout.spacing = new Vector2(0, 60);
+            teamsLayout.startAxis = GridLayoutGroup.Axis.Horizontal;
+        }
     }
 }
