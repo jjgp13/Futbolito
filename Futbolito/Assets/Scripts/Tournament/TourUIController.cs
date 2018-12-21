c4209b3e6cd1f91b04b556e6039ade1dbef337c8
José Juan Gálvez <jjgalper@gmail.com>
2018-12-21 13:22:11 -0600

    Menu Tour positions finished

--- Futbolito/Assets/Scripts/Tournament/TourUIController.cs
+++ Futbolito/Assets/Scripts/Tournament/TourUIController.cs
@@ -8,13 +8,24 @@ public class TourUIController : MonoBehaviour {
 
     public GameObject teamInfoUI;
     public GameObject positionsPanel;
+    public Text groupText;
+
     TournamentController tourInfo;
 
-	// Use this for initialization
-	void Start () {
-        tourInfo = GameObject.Find("TourController").GetComponent<TournamentController>();
+    public int teamsInTour;
+    public int firstTeamActiveInPanel;
+
+    private void Awake()
+    {
+        tourInfo = GameObject.FindGameObjectWithTag("TourData").GetComponent<TournamentController>();
+    }
+
+    // Use this for initialization
+    void Start () {
         SetTeamsPositionPanel(OrderTeamPositionList(tourInfo.teamList));
-	}
+        firstTeamActiveInPanel = 0;
+        groupText.text = "Group " + tourInfo.teamList[firstTeamActiveInPanel].group;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,14 +34,16 @@ public class TourUIController : MonoBehaviour {
 
     public void SetTeamsPositionPanel(List<TeamTourInfo> teamList)
     {
-        foreach (TeamTourInfo team in teamList)
+        for (int i = 0; i < teamsInTour; i++)
         {
             GameObject teamPosition = teamInfoUI;
-            Team teamInfo = Resources.Load<Team>("Teams/" + team.name + "/" + team.name);
+            Team teamInfo = Resources.Load<Team>("Teams/" + teamList[i].name + "/" + teamList[i].name);
             teamPosition.transform.GetChild(0).GetComponent<Image>().sprite = teamInfo.flag;
-            teamPosition.transform.GetChild(1).GetComponent<Text>().text = team.name;
+            teamPosition.transform.GetChild(1).GetComponent<Text>().text = teamList[i].name;
             Instantiate(teamPosition).transform.SetParent(positionsPanel.transform);
+            teamPosition.SetActive(false);
         }
+        for (int i = 0; i < 4; i++) positionsPanel.transform.GetChild(i).gameObject.SetActive(true);
     }
 
     public List<TeamTourInfo> OrderTeamPositionList(List<TeamTourInfo> teamList)
@@ -39,11 +52,52 @@ public class TourUIController : MonoBehaviour {
         for (int i = 0; i < teamList.Count; i+=4)
         {
             List<TeamTourInfo> groupU = new List<TeamTourInfo>();
-            for (int j = i; j < i+4; j++) groupU.Add(teamList[j]);
+            for (int j = i; j < i + 4; j++) groupU.Add(teamList[j]);
             List<TeamTourInfo> groupO = groupU.OrderBy(team => team.group).ToList();
-            for (int j = 0; j < groupO.Count; j++) teamsOrdered.Add(groupO[j]);
+            for (int k = 0; k < groupO.Count; k++) teamsOrdered.Add(groupO[k]);   
         }
-
+        teamsInTour = teamsOrdered.Count;
         return teamsOrdered;
+    }
+
+    public void ChangeGroupInPositionPanel(string direction)
+    {
+        if (direction == "left")
+        {
+            if (firstTeamActiveInPanel == 0)
+            {
+                firstTeamActiveInPanel = teamsInTour - 4;
+                ActivateTeamsInPanel(firstTeamActiveInPanel);
+            }
+            else
+            {
+                firstTeamActiveInPanel -= 4;
+                ActivateTeamsInPanel(firstTeamActiveInPanel);
+            }
+        }
+        if (direction == "right")
+        {
+            if (firstTeamActiveInPanel == teamsInTour - 4)
+            {
+                firstTeamActiveInPanel = 0;
+                ActivateTeamsInPanel(firstTeamActiveInPanel);
+            }
+            else
+            {
+                firstTeamActiveInPanel += 4;
+                ActivateTeamsInPanel(firstTeamActiveInPanel);
+            }
+        }
+        groupText.text = "Group " + tourInfo.teamList[firstTeamActiveInPanel].group;
+    }
+
+    private void ActivateTeamsInPanel(int iniPos)
+    {
+        for (int i = 0; i < teamsInTour; i++)
+        {
+            GameObject team = positionsPanel.transform.GetChild(i).gameObject;
+            if (i >= firstTeamActiveInPanel && i < firstTeamActiveInPanel + 4) team.SetActive(true);
+            else team.SetActive(false);
+        }
     }
 }
